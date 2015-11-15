23c72f142c3adccf80b20bef01d302625a118001
MalovaAA <nastyamalova1@yandex.ru>
2015-11-15 21:44:36 +0300

    добавила тесты на корректность скобок

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,2 +1,31 @@
 #include <gtest.h>
+#include "arithmetic.h"
+
+
+TEST(arithmetic, check_an_correctly_bracket1)
+{
+	string a;
+	arithmetic b,d;
+	a="a+b)";
+	b.SetFormula(a);
+	EXPECT_EQ(0,b.CheckFormula());
+}
+TEST(arithmetic,check_an_correctly_bracket2)
+{
+	string a;
+	arithmetic b,d;
+	a=")a+b()((";
+	b.SetFormula(a);
+	EXPECT_EQ(0,b.CheckFormula());
+}
+TEST(arithmetic,check_correctly_bracket)
+{
+	string a;
+	arithmetic b,d;
+	a="a+b*(c+b)";
+	b.SetFormula(a);
+	EXPECT_EQ(1,b.CheckFormula());
+}
+
+
 
