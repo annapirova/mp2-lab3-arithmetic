c4b5d324d9f8fd9ed79945235aee382f0ee5d763
DariaPanenkova <dariapanenkova@gmail.com>
2015-11-16 11:55:21 +0300

    Реализация шаблонного стека, добавлены тесты

--- include/stack.h
+++ include/stack.h
@@ -5,10 +5,11 @@
 
 #define MSize 20
 
+template <class ValType>
 class TStack
 {
 protected:
-	int *pMem;
+	ValType *pMem;
 	int Size;
 	int Index;
 public:
@@ -19,12 +20,80 @@ public:
 	bool StEmpty (void) const;
 	bool StFull (void) const;
 	
-	void PutElem(const int el); //добавить элемент в вершину стека 
-	int GetElem();              //удалить элемент из стека
+	void PutElem(const ValType &el); //добавить элемент в вершину стека 
+	ValType GetElem();              //удалить элемент из стека
 
 
 };
 
+template <class ValType>
+TStack<ValType>::TStack()
+{
+	Size = MSize;
+	pMem = new ValType[Size];
+	Index = -1;
 
+	for (int i = 0; i < Size; i++)
+	{
+		pMem[i] = 0;
+	}
+}
+
+template <class ValType>
+TStack<ValType>::TStack(int n)
+{
+	if (n<0)
+		throw("Negative size");
+	Size=n;
+	pMem= new ValType[Size];
+	Index = -1;
+	for (int i = 0; i < Size; i++) {
+		pMem[i] = 0;
+	}
+}
+
+template <class ValType>
+TStack<ValType>::~TStack()
+{
+	delete[]pMem;
+}
+
+template <class ValType>
+bool TStack<ValType>::StEmpty (void) const
+{
+	if(Index==-1)
+		return true;
+	return false;
+}
+
+template <class ValType>
+bool TStack<ValType>::StFull (void) const
+{
+	if( Index == Size-1)
+		return true;
+	return false;
+}
+
+template <class ValType>
+void TStack<ValType>::PutElem(const ValType &el)
+{
+	if ((*this).StFull())
+		throw ("Full stack");
+	Index+=1;
+	pMem[Index] = el;
+	
+}
+
+template <class ValType>
+ValType TStack<ValType>::GetElem()
+{
+	if ((*this).StEmpty())
+		throw ("Empty stack");
+	ValType a=pMem[Index];
+
+	pMem[--Index];
+
+	return a;
+}
 
 #endif
--- src/stack.cpp
+++ src/stack.cpp
@@ -1,60 +1,61 @@
-#include "stack.h"
-
-TStack::TStack()
-{
-	Size = MSize;
-	pMem = new int[Size];
-	Index = -1;
-
-	for (int i = 0; i < Size; i++)
-	{
-		pMem[i] = 0;
-	}
-}
-
-TStack::TStack(int n)
-{
-	if (n<0)
-		throw("Negative size");
-	Size=n;
-	pMem= new int[Size];
-	Index = -1;
-	for (int i = 0; i < Size; i++) {
-		pMem[i] = 0;
-	}
-}
-TStack::~TStack()
-{
-	delete[]pMem;
-}
-
-bool TStack::StEmpty (void) const
-{
-	if(Index==-1)
-		return true;
-	return false;
-}
-bool TStack::StFull (void) const
-{
-	if( Index == Size-1)
-		return true;
-	return false;
-}
-void TStack::PutElem(const int el)
-{
-	if ((*this).StFull())
-		throw ("Full stack");
-	
-	pMem[Index++] = el;
-}
-
-int TStack::GetElem()
-{
-	if ((*this).StEmpty())
-		throw ("Empty stack");
-	int a=pMem[Index];
-
-	pMem[--Index];
-
-	return a;
-}
+//#include "stack.h"
+//
+//TStack::TStack()
+//{
+//	Size = MSize;
+//	pMem = new int[Size];
+//	Index = -1;
+//
+//	for (int i = 0; i < Size; i++)
+//	{
+//		pMem[i] = 0;
+//	}
+//}
+//
+//TStack::TStack(int n)
+//{
+//	if (n<0)
+//		throw("Negative size");
+//	Size=n;
+//	pMem= new int[Size];
+//	Index = -1;
+//	for (int i = 0; i < Size; i++) {
+//		pMem[i] = 0;
+//	}
+//}
+//TStack::~TStack()
+//{
+//	delete[]pMem;
+//}
+//
+//bool TStack::StEmpty (void) const
+//{
+//	if(Index==-1)
+//		return true;
+//	return false;
+//}
+//bool TStack::StFull (void) const
+//{
+//	if( Index == Size-1)
+//		return true;
+//	return false;
+//}
+//void TStack::PutElem(const int el)
+//{
+//	if ((*this).StFull())
+//		throw ("Full stack");
+//	
+//	pMem[Index] = el;
+//	Index+=1;
+//}
+//
+//int TStack::GetElem()
+//{
+//	if ((*this).StEmpty())
+//		throw ("Empty stack");
+//	int a=pMem[Index];
+//
+//	pMem[--Index];
+//
+//	return a;
+//}
