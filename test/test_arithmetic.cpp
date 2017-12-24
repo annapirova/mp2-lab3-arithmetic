124d5e2b9d1485be1296dd24058d8edcb846fcd7
Angry Hedgehogg <mister.serega98@mail.ru>
2017-12-24 16:23:28 +0300

    testi

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -3,11 +3,21 @@
 #include <gtest.h>
 #include "arithmetic.h"
 
+
+string s1[22] = { "+++", "1+--2", "*/1", "1*", "3/", "(1+5", "1-5)", "(1+2)3", "3*(**2)","((2+3" };
+
+string s2[5] = { "1+2+3-(3-2-1)", "-5", "(-5)", "-(-(-1*2.3/(4+5.5)))", "1.5*2.5" };
+
+double s3[5] = { 1 + 2 + 3 - (3 - 2 - 1), -5, (-5), -(-(-1 * 2.3 / (4 + 5.5))), 1.5 *2.5 };
+
+
 TEST(Lexem, can_create_lexem_val)
 {
 	Lexem L("2");
 	EXPECT_EQ(2, L.val);
 }
+
+
 
 TEST(Lexem, can_define_type_of_lexem)
 {
@@ -15,7 +25,6 @@ TEST(Lexem, can_define_type_of_lexem)
 	Lexem L(s);
 	EXPECT_EQ(L.type, VAL);
 }
-
 
 TEST(Lexem, can_calc)
 {
@@ -25,6 +34,27 @@ TEST(Lexem, can_calc)
 	double res = A.Calculate();
 	EXPECT_EQ(6, res);
 }
+
+TEST(Lexem, can_calc_2)
+{
+	Arithmetic A("(1-2*3)*(1-3)");
+	A.DivideToLexems();
+	A.PolishNotation();
+	double res = A.Calculate();
+	EXPECT_EQ(10, res);
+}
+
+
+TEST(Arithmetic, convert_to_polish_notation)
+{
+	Arithmetic A("2*12");
+	A.DivideToLexems();
+	A.PolishNotation();
+	EXPECT_EQ(2, A.GetValplLexems(0));
+	EXPECT_EQ(12, A.GetValplLexems(1));
+	EXPECT_EQ(2, A.GetValplLexems(2));
+}
+
 
 TEST(Arithmetic, check_brack)
 {
@@ -37,12 +67,22 @@ TEST(Arithmetic, check_brack)
 
 TEST(Arithmetic, check_brack_2)
 {
-	Arithmetic B("(1+2-2-1");
+	Arithmetic A("(1+2-2-1");
+	bool r1;
+	A.DivideToLexems();
+	r1 = A.CheckBrackets();
+	EXPECT_EQ(false, r1);
+}
+
+TEST(Arithmetic, check_brack_3)
+{
+	Arithmetic A("()(1+21))");
 	bool r2;
-	B.DivideToLexems();
-	r2 = B.CheckBrackets();
+	A.DivideToLexems();
+	r2 = A.CheckBrackets();
 	EXPECT_EQ(false, r2);
 }
+
 
 TEST(Arithmetic, check_operator)
 {
@@ -64,13 +104,63 @@ TEST(Arithmetic, check_operator_2)
 }
 
 
-TEST(Arithmetic, polish_notation)
+TEST(Arithmetic, check_operator_3)
+{
+	Arithmetic A("(2+3)+(-1)");
+	bool r;
+	A.DivideToLexems();
+	r = A.CheckOperators();
+	EXPECT_EQ(true, r);
+}
+
+
+TEST(Arithmetic, can_divide_to_lexems)
 {
 	Arithmetic A("2*12");
 	A.DivideToLexems();
-	A.PolishNotation();
-	EXPECT_EQ(2, A.GetValplLexems(0));
-	EXPECT_EQ(12, A.GetValplLexems(1));
-	EXPECT_EQ(2, A.GetValplLexems(2));
+	EXPECT_EQ(2, A.GetValLexems(0));
+	EXPECT_EQ(2, A.GetValLexems(1));
+	EXPECT_EQ(12, A.GetValLexems(2));
+}
+
+TEST(Arithmetic, uncorrect_string)
+{
+	for (int i = 0; i < 11; i++)
+	{
+		Arithmetic A(s1[i]);
+		A.DivideToLexems();
+		EXPECT_EQ(false, A.CheckBrackets() && A.CheckOperators());
+	}
+}
+
+
+TEST(Arithmetic, check_points)
+{
+	Arithmetic A("2.5");
+	A.DivideToLexems();
+	EXPECT_EQ(1, A.CheckPoints("2.5"));
+}
+
+
+TEST(Arithmetic, correct_string)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Arithmetic A(s2[i]);
+		A.DivideToLexems();
+		EXPECT_EQ(true, A.CheckBrackets() && A.CheckOperators());
+	}
+}
+
+ 
+TEST(Arithmetic, can_calculate_2)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Arithmetic A(s2[i]);
+		A.DivideToLexems();
+		A.PolishNotation();
+		EXPECT_EQ(s3[i], A.Calculate());
+	}
 }
 
