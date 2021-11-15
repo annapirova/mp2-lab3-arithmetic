d2e573a23e3c0423e694f2a74919ce121e9e4495
Виктория Суслова <73444769+vikkkkos@users.noreply.github.com>
2021-11-15 22:55:26 +0300

    add 1ver

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,27 @@
-// объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#ifndef _ARITHMETIC_H_
+#define _ARITHMETIC_H_
+#include "stack.h"
+#include <string>
+
+class Arithmetic
+{
+private:
+    
+public:
+    string s;
+    string* Lex_in;
+    string polish;
+    int type;
+    Stack<char> res;
+    Stack<char> operation;
+    int size;
+
+    void divided_lex();
+    bool check_brackets();
+    //int check_formula();
+    int prioritet(string s);
+    void polsk_zap();
+    bool calculating();
+};
+
+#endif
