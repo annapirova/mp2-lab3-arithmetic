26725e9b27846d8348f872ec300fbac752e0396c
Виктория Суслова <73444769+vikkkkos@users.noreply.github.com>
2021-11-17 22:18:51 +0300

    add 1v

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,20 @@
-// реализация пользовательского приложения
+﻿#include "../arithmetic1/arithmetic.h"
+#include <string>
+
+#include <iostream>
 
 int main()
 {
-  return 0;
+    string str = " ";
+    cout << "Enter :" << endl;
+    getline(cin, str);
+    pair<char, int> m;
+    Arithmetic A;
+   // string str1;
+    A.divided_lex();
+    A.polsk_zap();
+    //cout << "Польская запись: " << A.divided_lex() << endl;
+    double res;
+    res = A.calculating();
+    cout << "Result: " << res << endl;
 }
