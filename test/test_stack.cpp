6268e9c9328a48b5ba6162f8b534729a0af2c99f
Виктория Суслова <73444769+vikkkkos@users.noreply.github.com>
2021-12-09 18:05:44 +0300

    т

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -16,10 +16,23 @@ TEST(Stack, can_create_stack_with_too_large_length)
     ASSERT_NO_THROW(Stack<int>, T(MAX_STACK_SIZE));
 }
 
+TEST(Stack, can_create_copied_stack)
+{
+    Stack<int> st(3);
+    ASSERT_NO_THROW(Stack<int> st1(st));
+}
+
 TEST(Stack, stack_is_empty)
 {
-    Stack<int> T(3);
-    EXPECT_TRUE(T.IsEmpty());
+    Stack<int> st(3);
+    EXPECT_EQ(true, st.IsEmpty());
+}
+
+TEST(Stack, stack_is_not_empty)
+{
+    Stack<int> st(3);
+    st.Push(1);
+    EXPECT_EQ(false, st.IsEmpty());
 }
 
 TEST(Stack, can_push_element_in_stack)
@@ -30,24 +43,32 @@ TEST(Stack, can_push_element_in_stack)
 
 TEST(Stack, can_see_top_element)
 {
-    Stack<int> T(3);
-    ASSERT_NO_THROW(Stack<int>, T.Peek());
+    Stack<int> st(3);
+    st.Push(1);
+    EXPECT_EQ(1, st.Peek());
+    //ASSERT_NO_THROW(Stack<int>, T.Peek());
 }
 
 TEST(Stack, can_clear)
 {
-    Stack<int> T(3);
-    ASSERT_NO_THROW(Stack<int>, T.Clear());
+    Stack<int> st(3);
+    st.Push(1);
+    st.Clear();
+    EXPECT_TRUE(st.IsEmpty());
 }
 
 TEST(Stack, can_get_the_size_of_stack)
 {
     Stack<int> T(3);
-    ASSERT_NO_THROW(Stack<int>, T.GetSize());
+    EXPECT_EQ(3, T.GetSize());
+    //ASSERT_NO_THROW(Stack<int>, T.GetSize());
 }
 TEST(Stack, can_delete_element_from_stack)
 {
-    Stack<int> T(3);
-    ASSERT_NO_THROW(Stack<int>, T.Pop());
+    Stack<int> st(3);
+    st.Push(1);
+    st.Push(2);
+    st.Push(3);
+    ASSERT_NO_THROW(3, st.Pop());
 }
 
